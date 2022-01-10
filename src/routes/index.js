d3adf5ef7abd14ea25bb821b1eea5d8093223e7c
CEmmanuelP <chrisp3@daum.net>
2022-01-11 06:52:04 +0900

    houses.js -header & filter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,27 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import IndexPage from "../pages"
-import HomePage from "../pages/home"
-import LoginPage from "../pages/login"
-import NoMatchPage from "../pages/noMatch"
-
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import IndexPage from "../pages";
+import HomePage from "../pages/home";
+import HousesPage from "../pages/houses";
+import LoginPage from "../pages/login";
+import NoMatchPage from "../pages/noMatch";
 
 const RootRoute = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* 기본 루트페이지 */}
+        <Route path="/" component={IndexPage} exact key="index" />
 
-    return (
-        <BrowserRouter>
-            <Switch>
+        {/* 추가되는 페이지 */}
+        <Route path="/home" component={HomePage} exact key="home" />
+        <Route path="/login" component={LoginPage} exact key="login" />
+        <Route path="/houses" component={HousesPage} />
 
-                {/* 기본 루트페이지 */}
-                <Route path='/' component={IndexPage} exact key="index" />
+        {/* 경로가 유효하지 않을 때 */}
+        <Route path="*" component={NoMatchPage} key="noMatch" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-                {/* 추가되는 페이지 */}
-                <Route path='/home' component={HomePage} exact key="home" />
-                <Route path='/login' component={LoginPage} exact key="loign" />
-
-                {/* 경로가 유효하지 않을 때 */}
-                <Route path='*' component={NoMatchPage} key="noMatch" />
-
-            </Switch>
-        </BrowserRouter>
-    )
-}
-
-
-export default RootRoute
+export default RootRoute;
