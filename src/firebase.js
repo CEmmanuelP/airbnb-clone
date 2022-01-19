6d149d923cb9237f60f13552171f58298238e260
CEmmanuelP <chrisp3@daum.net>
2022-01-19 17:16:27 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAvQPHJGW9f4L0ONG68MU0toElOaXmOz04",
+    authDomain: "web-airbnb-clone.firebaseapp.com",
+    projectId: "web-airbnb-clone",
+    storageBucket: "web-airbnb-clone.appspot.com",
+    messagingSenderId: "866045920537",
+    appId: "1:866045920537:web:38ae65f3f99543cca73188",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
